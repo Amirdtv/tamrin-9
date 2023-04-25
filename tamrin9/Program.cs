71694a2fb8487c1ec658a75a127b7856e61685d1
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-04-25 11:42:04 +0330

    s

---
+++ tamrin9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin10
+{
+    internal class Program
+    {
+        private static void test ( int[ , ] a)
+        {
+            int i, j;
+            for (i = 0; i <= 49; i++)
+            {
+                for(j = 1; j <=48; j++)
+                {
+                    a[i, j] = (a[i, j - 1] + a[i, j] + a[i, j + 1])/3;
+                }
+            }
+            for (i = 0; i <= 49; i++)
+            {
+                for (j = 0; j <= 49; j++)
+                {
+                    Console.WriteLine(a[i, j]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[ , ] a = new int[50 , 50];
+            test( a );
+        }
+    }
+}
